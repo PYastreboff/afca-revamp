d72186485d0af7b39592b524e7dd5dd304e5e3a8
PYastreboff <PYastreboff@users.noreply.github.com>
2026-06-14 09:06:09 +0000

    deploy: a4057f900f353f2d937e7d171351df9856f5ca8b

---
+++ _next/static/hF-f4dxe9KKe7zXdds3Jz/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/afca-revamp//_next/:path+",
+        "destination": "/afca-revamp/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
